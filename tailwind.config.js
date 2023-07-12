ebb3070a6422d845e2fcd608022bd74234230a19
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-12 01:36:46 -0300

    Tags funcionais para a palheta de cores e a tabela de tipografia.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        c1: "#633CFF",
+        c2: "#BEADFF",
+        c3: "#EFEBFF",
+        c4: "#333333",
+        c5: "#737373",
+        c6: "#D9D9D9",
+        c7: "#FAFAFA",
+        c8: "#FFFFFF",
+        c9: "#FF3939",
+      },
+    },
   },
   plugins: [],
 };

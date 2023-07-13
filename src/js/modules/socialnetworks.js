fe2cb322b8ef50f1c59331f283e86faef37a8d0d
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-07-13 17:43:07 -0300

    Correção das classes funcionais e adição do ENUM socialnetworks.js com as informações sobre o estilo de cada rede social.

---
+++ src/js/modules/socialnetworks.js
@@ -0,0 +1,92 @@
+const socialnetworks = {
+  facebook: {
+    name: "Facebook",
+    path: "./assets/images/icon-facebook.svg",
+    bg: "#2442AC",
+    color: "#ffffff",
+  },
+  codepen: {
+    name: "Codepen",
+    path: "./assets/images/icon-codepen.svg",
+    bg: "#8A1A50",
+    color: "#ffffff",
+  },
+  codewars: {
+    name: "Facebook",
+    path: "./assets/images/icon-codewars.svg",
+    bg: "#8A1A50",
+    color: "#ffffff",
+  },
+  devto: {
+    name: "Dev.to",
+    path: "./assets/images/icon-devto.svg",
+    bg: "#333333",
+    color: "#ffffff",
+  },
+  email: {
+    name: "Email",
+    path: "./assets/images/icon-email.svg",
+    bg: "#8A1A50",
+    color: "#ffffff",
+  },
+  freecodecamp: {
+    name: "FreeCodeCamp",
+    path: "./assets/images/icon-freecodecamp.svg",
+    bg: "#302267",
+    color: "#ffffff",
+  },
+  github: {
+    name: "GitHub",
+    path: "./assets/images/icon-github.svg",
+    bg: "#1A1A1A",
+    color: "#ffffff",
+  },
+  gitlab: {
+    name: "GitLab",
+    path: "./assets/images/icon-gitlab.svg",
+    bg: "#EB4925",
+    color: "#ffffff",
+  },
+  hashnode: {
+    name: "Hashnode",
+    path: "./assets/images/icon-hashnode.svg",
+    bg: "#0330D1",
+    color: "#ffffff",
+  },
+  linkedin: {
+    name: "LinkedIn",
+    path: "./assets/images/icon-linkedin.svg",
+    bg: "#2D68FF",
+    color: "#ffffff",
+  },
+  stackoverflow: {
+    name: "Stack Overflow",
+    path: "./assets/images/icon-stack-overflow.svg",
+    bg: "#EC7100",
+  },
+  twitch: {
+    name: "Twitch",
+    path: "./assets/images/icon-twitch.svg",
+    bg: "#8A1A50",
+    color: "#ffffff",
+  },
+  twitter: {
+    name: "Twitter",
+    path: "./assets/images/icon-twitter.svg",
+    bg: "#43B7E9",
+    color: "#ffffff",
+  },
+  youtube: {
+    name: "Youtube",
+    path: "./assets/images/icon-youtube.svg",
+    bg: "#EE3939",
+    color: "#ffffff",
+  },
+  frontendmentor: {
+    name: "Frontend Mentor",
+    path: "./assets/images/icon-frontend-mentor.svg",
+    bg: "#ffffff",
+    color: "#000000",
+  },
+};
+export default socialnetworks;
